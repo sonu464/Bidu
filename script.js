f058fe34c5648b5dbfca11fec7655d7a51b169b5
sonu464 <122866843+sonu464@users.noreply.github.com>
2023-05-04 15:14:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let hemburger = document.querySelector('.hemburger');
 let line = document.querySelector('.line');
 let navigation = document.getElementById('navigation');
-let cross = document.getElementById('cross');
+let hemburger1 = document.querySelector('.hemburger1');
 function hem() { 
 
   navigation.style.height="100vh";
   navigation.style.transition="all 1s";
   hemburger.style.display= "none";
-  cross.style.display= "flex";
+  hemburger1.style.display= "flex";
 }
