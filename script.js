4f09f822a9d9f255cae3f048ba0e9624e94fcc0a
sonu464 <122866843+sonu464@users.noreply.github.com>
2023-05-04 13:22:58 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let hemburger = document.querySelector('.hemburger');
+let line = document.querySelector('.line');
+
+function hem() { 
+  hemburger.style.height="100vh";
+  hemburger.style.transition="all 1s";
+}
