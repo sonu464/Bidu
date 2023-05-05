6436d71a558e398abb5628f783c49b91d19e26e9
sonu464 <122866843+sonu464@users.noreply.github.com>
2023-05-05 07:11:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let hemburger1 = document.querySelector('.hemburger1');
 let navRight = document.querySelector('.nav-right');
 
 
-if(navigation.style.height===50px){
+if(navigation.style.height === 50px){
 function hem() { 
   navigation.style.height="100vh";
   navigation.style.transition="all 1s";
