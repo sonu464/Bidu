f0ed6aeeeba9e1f787aa857a92278c34f525ec61
sonu464 <122866843+sonu464@users.noreply.github.com>
2023-05-05 06:23:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function hem() {
   navigation.style.transition="all 1s";
   hemburger.style.display= "none";
   hemburger1.style.display= "flex";
-  // navRight.style.display= "";
-  // navRight.style.flexDirection= "column";
+  navRight.style.display= "";
+  navRight.style.flexDirection= "column";
   navRight.element.classList.toggle("nav-right1");
 }
 
